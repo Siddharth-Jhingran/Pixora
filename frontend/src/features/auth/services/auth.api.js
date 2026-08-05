81d04bd1d020b1c87c25089529d58ca0ccf930a4
Siddharth Jhingran <siddharthjhingran29@gmail.com>
2026-08-05 19:18:57 +0530

    feat: update API base URLs to use production environment

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/api/auth",
+    baseURL:"https://pixora-8gz8.onrender.com/api/auth",
     withCredentials:true
 })
 
